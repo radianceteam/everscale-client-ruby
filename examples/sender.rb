0ee64df7dfefc1395c7236130a294a6cbf63a506
alex maslakoff <alex@serendipia.email>
2020-12-12 16:57:46 +0530

    wip

--- examples/sender.rb
+++ examples/sender.rb
@@ -1,5 +1,6 @@
 require_relative './examples_helper.rb'
 
+# TODO
 
 cont_json = File.read("#{EXAMPLES_DATA_DIR}/contracts/abi_v2/Transfer.abi.json")
 abi1 = TonSdk::Abi::Abi.new(
@@ -7,38 +8,54 @@ abi1 = TonSdk::Abi::Abi.new(
   value: TonSdk::Abi::AbiContract.from_json(JSON.parse(cont_json))
 )
 
-signing = TonSdk::Abi::Signer.new(type_: :external, public_key: keys.public_)
-pr1 = TonSdk::Abi::ParamsOfEncodeMessage.new(
+pr_s = TonSdk::Abi::ParamsOfEncodeMessageBody.new(
   abi: abi1,
   call_set: TonSdk::Abi::CallSet.new(
-    function_name: "constructor",
-    header: TonSdk::Abi::FunctionHeader.new(
-      pubkey: keys.public_,
-      time: time1,
-      expire: expire1,
-    ),
+    function_name: "transfer",
+    input: {
+      "comment": "howdy!!!1"
+    }
   ),
-  signer: signing
+  is_internal: true,
+  signer: nil
 )
 
-
-
-
-
-
-
-cont_json = File.read("#{EXAMPLES_DATA_DIR}/contracts/abi_v2/Events.abi.json")
-abi1 = TonSdk::Abi::Abi.new(
-  type_: :contract,
-  value: TonSdk::Abi::AbiContract.from_json(JSON.parse(cont_json))
-)
-
-tvc1_cont_bin = IO.binread("#{EXAMPLES_DATA_DIR}/contracts/abi_v2/Events.tvc")
-tvc1 = Base64::strict_encode64(tvc1_cont_bin)
-
-
-TonSdk::Crypto::generate_random_sign_keys(@c_ctx.context) do |res|
+TonSdk::Abi::encode_message_body(@graphql_c_ctx, pr_s) do |res|
   if res.success?
-    @keys = res.result
+    cont_json2 = File.read("#{EXAMPLES_DATA_DIR}/contracts/abi_v2/SetcodeMultisigWallet.abi.json")
+    abi2 = TonSdk::Abi::Abi.new(
+      type_: :contract,
+      value: TonSdk::Abi::AbiContract.from_json(JSON.parse(cont_json2))
+    )
+
+    enc_pr_s = TonSdk::Abi::ParamsOfEncodeMessage.new(
+      abi: abi2,
+      address: sen_addr,
+      call_set: TonSdk::Abi::CallSet.new(
+        function_name: "sendTransaction",
+        input: {
+          "dest": rec_addr,
+          "value": 12345000,
+          "bounce": false,
+          "flags": 3,
+          "payload": res.result
+        }
+      ),
+      signer: TonSdk::Abi::Signer.new(type_: :keys, keys: keys)
+    )
+
+    pr_s2 = ParamsOfProcessMessage.new(
+      message_encode_params: enc_pr_s,
+      send_events: true
+    )
+
+    cb = Proc.new do |a|
+      puts "message: #{a}"
+    end
+
+    TonSdk::Processing::process_message(@graphql_c_ctx, pr_s2, cb) do |res2|
+
+    end
+
   end
 end
