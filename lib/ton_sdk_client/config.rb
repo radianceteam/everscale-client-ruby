f198a86339580ecd9c8e4574aa35f8be09d3cc92
alex maslakov <alex@serendipia.email>
2021-01-12 22:08:17 +0200

    wip

--- lib/ton_sdk_client/config.rb
+++ lib/ton_sdk_client/config.rb
@@ -26,31 +26,45 @@ module TonSdk
     DEFAULT_PROCESSING_TIMEOUT = 40000
     DEFAULT_WAIT_TIMEOUT = 40000
     DEFAULT_OUT_OF_SYNC_THRESHOLD = 15000
+    DEFAULT_NETWORK_RETRIES_COUNT = 5
 
-    attr_reader :server_address, :message_retries_count, :message_processing_timeout,
-      :wait_for_timeout, :out_of_sync_threshold, :access_key
+    attr_reader :server_address, :endpoints, :network_retries_count,
+      :message_retries_count, :message_processing_timeout,
+      :wait_for_timeout, :out_of_sync_threshold, :reconnect_timeout,
+      :access_key
 
     def initialize(
       server_address: "",
+      endpoints: [],
+      network_retries_count: DEFAULT_NETWORK_RETRIES_COUNT,
       message_retries_count: DEFAULT_RETRIES_COUNT,
       message_processing_timeout: DEFAULT_PROCESSING_TIMEOUT,
       wait_for_timeout: DEFAULT_WAIT_TIMEOUT,
-      out_of_sync_threshold: DEFAULT_OUT_OF_SYNC_THRESHOLD
+      out_of_sync_threshold: DEFAULT_OUT_OF_SYNC_THRESHOLD,
+      reconnect_timeout: 0,
+      access_key: nil
     )
       @server_address = server_address
+      @endpoints = endpoints
+      @network_retries_count = network_retries_count
       @message_retries_count = message_retries_count
       @message_processing_timeout = message_processing_timeout
       @wait_for_timeout = wait_for_timeout
       @out_of_sync_threshold = out_of_sync_threshold
+      @reconnect_timeout = reconnect_timeout
+      @access_key = access_key
     end
 
     def to_h
       {
         server_address: @server_address,
+        endpoints: @endpoints,
+        network_retries_count: @network_retries_count,
         message_retries_count: @message_retries_count,
         message_processing_timeout: @message_processing_timeout,
         wait_for_timeout: @wait_for_timeout,
         out_of_sync_threshold: @out_of_sync_threshold,
+        reconnect_timeout: @reconnect_timeout,
         access_key: @access_key
       }
     end
